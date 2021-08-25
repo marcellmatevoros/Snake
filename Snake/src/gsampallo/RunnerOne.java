1a752520f697eb583bbbb4d05d8e4477936973b9
Marcell Vörös <85932631+marcellmatevoros@users.noreply.github.com>
2021-08-25 20:10:08 +0200

    Add files via upload

---
+++ Snake/src/gsampallo/RunnerOne.java
@@ -0,0 +1,14 @@
+package gsampallo;
+
+public class RunnerOne {
+
+
+	public RunnerOne(){
+		System.out.println("Runner One !");
+
+	}
+
+	public static void main(String[] args) throws Exception {
+		RunnerOne runnerOne = new RunnerOne();
+	}
+}
